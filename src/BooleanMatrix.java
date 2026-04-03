c205c3501062ef309a98febb846dbc450df49276
Saranya-Ramadasan <rsnyaramadasan04@gmail.com>
2026-04-03 14:49:13 +0530

    first commit

--- src/BooleanMatrix.java
+++ src/BooleanMatrix.java
@@ -1,14 +1,47 @@
+
+import java.util.Scanner;
 public class BooleanMatrix {
     
-    // TODO: Implement the function to modify the matrix
-    
-        // Logic: Check each row. If it contains a 1, set the whole row to 1.
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
 
-    
-        // TODO: Read m and n
-        // TODO: Read the matrix elements
-        // TODO: Call BooleanMatrixProblem(mat, m, n)
-        // TODO: Print the modified matrix
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] mat = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        BooleanMatrixProblem(mat, m, n);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(mat[i][j] + (j == n - 1 ? "" : " "));
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    public static void BooleanMatrixProblem(int[][] mat, int m, int n) {
+        for (int i = 0; i < m; i++) {
+            boolean hasOne = false;
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    hasOne = true;
+                    break;
+                }
+            }
+            if (hasOne) {
+                for (int j = 0; j < n; j++) {
+                    mat[i][j] = 1;
+                }
+            }
+        }
+    }
     
 }
